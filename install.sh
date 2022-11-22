11b3addf8dcef952c2b3d69772ad7508c0a68712
Michael Grijalva <michael.grijalva@forgeglobal.com>
2022-11-22 12:04:42 -0800

    Get rid of vim

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install vim
-
 git config --global alias.co checkout
 git config --global alias.s status
 git config --global alias.p pull
 git config --global alias.pwf push --force-with-lease
-
-mv .vimrc ~/.vimrc
