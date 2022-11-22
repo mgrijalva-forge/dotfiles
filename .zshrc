88f9da9482997cdd314601f6857144757cfe4c9b
Michael Grijalva <michael.grijalva@forgeglobal.com>
2022-11-22 12:16:59 -0800

    aliases

---
+++ .zshrc
@@ -0,0 +1,5 @@
+alias cd_monolith='cd /workspaces/shagorex/sharex/monolith'
+
+alias ms='cd_monolith && rail s'
+alias my='cd_monolith && yarn start'
+alias mj='cd_monolith && rails jobs:work'
--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ git config --global alias.co checkout
 git config --global alias.s status
 git config --global alias.p pull
 git config --global alias.pwf push --force-with-lease
+
+cat .zshrc >> ~/.zshrc
