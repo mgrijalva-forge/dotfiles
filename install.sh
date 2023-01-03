e5afa960b3a674df9d2abcbee3f33f5a2321d90c
mgrijalva-forge <michael.grijalva@forgeglobal.com>
2023-01-03 10:29:38 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ git config --global alias.p pull
 git config --global alias.pwf push --force-with-lease
 
 cat .zshrc >> ~/.zshrc
+
+# Switch back to private ports
+echo 'export ASSET_HOST="http://127.0.0.1:5000"' >> /workspaces/shagorex/sharex/monolith/.env.local
+echo 'export LOCALSTACK_ENDPOINT="http://127.0.0.1:4566"' >> /workspaces/shagorex/sharex/monolith/.env.local
+echo 'export WEBPACKER_DEV_SERVER_PUBLIC="http://127.0.0.1:3035"' >> /workspaces/shagorex/sharex/monolith/.env.local
+echo 'export OKTA_CLIENT_OPTIONS_CALLBACK="http://127.0.0.1:5000/auth/callback?provider=okta"' >> /workspaces/shagorex/sharex/monolith/.env.local
